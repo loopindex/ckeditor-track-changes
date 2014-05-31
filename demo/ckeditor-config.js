1b4df7a16575d1ac1ad18b7fe5258e32da0897b3
imdfl <dfl.github@gmail.com>
2014-05-31 22:59:35 +0300

    fixed include in config

--- demo/ckeditor-config.js
+++ demo/ckeditor-config.js
@@ -17,7 +17,7 @@ CKEDITOR.editorConfig = function( config ) {
 	]; */
 	
 	var lite = config.lite = config.lite || {};
-	config.extraPlugins = 'divarea,lite';
+	config.extraPlugins = 'lite';
 	lite.includes_debug = ["js/rangy/rangy-core.js", "js/ice.js", "js/dom.js", "js/selection.js", "js/bookmark.js","lite_interface.js"];
 	lite.includes = ["lite-includes.min.js"];
 	lite.includes_full = ["lite-includes.js"];
