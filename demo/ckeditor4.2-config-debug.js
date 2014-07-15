917c857630c582469960125b2f5166afc5cb3a71
imdfl <dfl.github@gmail.com>
2014-07-15 18:15:06 +0300

    Added ckeditor 4.2 config

---
+++ demo/ckeditor4.2-config-debug.js
@@ -0,0 +1,23 @@
+CKEDITOR.editorConfig = function( config ) {
+	var lite = config.lite = config.lite || {};
+	config.extraPlugins = 'lite';
+	lite.includes  = ["js/rangy/rangy-core.js", "js/ice.js", "js/dom.js", "js/selection.js", "js/bookmark.js","lite_interface.js"];
+	lite.isTracking = true;
+	lite.userStyles = {
+		15: 1,
+		18:2,
+		21:3
+	};
+	// these are the default tooltip values. If you want to use this default configuration, just set lite.tooltips = true;
+	lite.tooltips = {
+		show: true,
+		path: "js/opentip-adapter.js",
+		classPath: "OpentipAdapter",
+		cssPath: "css/opentip.css",
+		delay: 500
+	};
+//	lite.commands = [/*LITE.Commands.TOGGLE_TRACKING, */LITE.Commands.TOGGLE_SHOW/*, LITE.Commands.ACCEPT_ALL, LITE.Commands.REJECT_ALL, LITE.Commands.ACCEPT_ONE, LITE.Commands.REJECT_ONE */];
+	config.enterMode = CKEDITOR.ENTER_BR;
+	config.autoParagraph = false;
+	config.title = false;
+};
