d91824d6bf12796220c7d81050ec75a7313874a7
imdfl <dfl.github@gmail.com>
2015-04-08 15:28:26 +0300

    Added ant tasks

--- src/lite/plugin.js
+++ src/lite/plugin.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2013 LoopIndex, This file is part of the Track Changes plugin for CKEditor.
+Copyright 2015 LoopIndex, This file is part of the Track Changes plugin for CKEditor.
 
 The track changes plugin is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License, version 2, as published by the Free Software Foundation.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
