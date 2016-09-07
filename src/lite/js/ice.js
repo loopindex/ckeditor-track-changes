d81ad026d92e26408a6545484d8ac82e3be79262
Grzegorz Hnat <ghnat@smpl.pl>
2016-09-07 14:28:54 +0200

    when removing text selection that includes ckeditor widgets, remove the widgets instead of trying to mark them deleted with <del>

--- src/lite/js/ice.js
+++ src/lite/js/ice.js
@@ -1485,7 +1485,7 @@
 					}
 				}
 				// Ignore empty space nodes
-				if (ice.dom.isEmptyTextNode(elem)) {
+				if (ice.dom.isEmptyTextNode(elem) || elem.getAttribute('data-cke-widget-wrapper') === '1') {
 					this._removeNode(elem);
 					continue;
 				}
